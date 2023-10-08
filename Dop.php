f54af5a5427013c62d3b28f35bb81342ac426e52
Alexsnder <krut.pitt@yandex.ru>
2023-10-09 02:24:50 +0300

    Sem_4

---
+++ Dop.php
@@ -0,0 +1,8 @@
+<?php
+    $_profession = 'Разработчик';
+    $_city_country = 'Мюнхен, Германия';
+    $_email = 'krut.pitt@yandex.ru';
+    $_mobile = '+48 236 144 321';
+
+    echo $_profession
+?>
